3e24901a98d3818304af7410c8fbe745cb8ddbbc
teotoivo <teo.maximilien@gmail.com>
2025-05-05 16:18:14 +0300

    aa

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -7,14 +7,11 @@
 		pkgs.fzf
 	];
 
-home.file.".p10k.zsh".source = ./p10k.zsh;
+	home.file.".p10k.zsh".source = ./p10k.zsh;
 
 	# Symlink powerlevel10k where oh-my-zsh expects it
 	home.file.".oh-my-zsh/custom/themes/powerlevel10k".source =
 		"${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k";
-
-home.file.".zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh".source =
-	./home/extras/zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh;
 
 	programs.zsh = {
 		enable = true;
@@ -36,26 +33,29 @@ home.file.".zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh".source =
 		# Prevent sourcing global zshrc (reproducibility)
 		initExtraBeforeCompInit = ''
 			unsetopt GLOBAL_RCS
-      export ZSH_CUSTOM="$HOME/.oh-my-zsh/custom/"
+			export ZSH_CUSTOM="$HOME/.oh-my-zsh/custom/"
 		'';
 
 		# Environment and plugin bootstrapping
 		initExtra = ''
-			# fzf plugin requires this
-			export FZF_BASE="${pkgs.fzf}/share/fzf"
-			[ -f "$FZF_BASE/shell/key-bindings.zsh" ] && source "$FZF_BASE/shell/key-bindings.zsh"
-			[ -f "$FZF_BASE/shell/completion.zsh" ] && source "$FZF_BASE/shell/completion.zsh"
+    # fzf plugin requires this
+	export FZF_BASE="${pkgs.fzf}/share/fzf"
+	[ -f "$FZF_BASE/shell/key-bindings.zsh" ] && source "$FZF_BASE/shell/key-bindings.zsh"
+	[ -f "$FZF_BASE/shell/completion.zsh" ] && source "$FZF_BASE/shell/completion.zsh"
 
-			# General env
-			export EDITOR=nvim
-			export PATH="$HOME/.local/bin:$PATH"
+	# General env
+	export EDITOR=nvim
+	export PATH="$HOME/.local/bin:$PATH"
 
-      [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
-
-      if [ -f "$HOME/.zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh" ]; then
-		    source "$HOME/.zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh"
-      fi
+	# powerlevel10k
+	[[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
 		'';
 	};
-}
 
+
+
+    home.file.".zshrc".text = ''
+    # Enable syntax highlighting with Catppuccin colors
+    source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+  '';
+}
