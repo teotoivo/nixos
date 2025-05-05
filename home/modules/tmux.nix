e82c9b6c12308f32480a4fd78adf323df26cf292
teotoivo <teo.maximilien@gmail.com>
2025-05-05 03:09:01 +0300

    a

--- home/modules/tmux.nix
+++ home/modules/tmux.nix
@@ -8,6 +8,7 @@
 		mouse = true;
 		prefix = "C-s";
 		terminal = "screen-256color";
+
 		extraConfig = ''
 			unbind r
 			bind r source-file ~/.config/tmux/tmux.conf
@@ -39,17 +40,13 @@
 			bind-key -n 'C-j' if-shell "$is_vim" 'send-keys C-j'  'select-pane -D'
 			bind-key -n 'C-k' if-shell "$is_vim" 'send-keys C-k'  'select-pane -U'
 			bind-key -n 'C-l' if-shell "$is_vim" 'send-keys C-l'  'select-pane -R'
-			tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\\1/p")'
-			if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' \
-				"bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\'  'select-pane -l'"
-			if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' \
-				"bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\\\'  'select-pane -l'"
+			bind-key -n 'C-;' if-shell "$is_vim" 'send-keys C-;'  'select-pane -l'
 
 			bind-key -T copy-mode-vi 'C-h' select-pane -L
 			bind-key -T copy-mode-vi 'C-j' select-pane -D
 			bind-key -T copy-mode-vi 'C-k' select-pane -U
 			bind-key -T copy-mode-vi 'C-l' select-pane -R
-			bind-key -T copy-mode-vi 'C-\\' select-pane -l
+			bind-key -T copy-mode-vi 'C-;' select-pane -l
 		'';
 	};
 
@@ -58,11 +55,11 @@
 		pkgs.fetchFromGitHub {
 			owner = "catppuccin";
 			repo = "tmux";
-			rev = "HEAD"; # pin to a specific commit if needed
-			sha256 = "16g7sfxvfy2j3zk27winx5g4jxmz7i02rhbfzjhkxnih590bg0d6"; # replace with actual
+			rev = "HEAD";
+			sha256 = "16g7sfxvfy2j3zk27winx5g4jxmz7i02rhbfzjhkxnih590bg0d6";
 		} + "/catppuccin.tmux";
 
-	# Plugin config (nonstandard @variables must go in a separate file or inline in `extraConfig`)
+	# Plugin config
 	xdg.configFile."tmux/tmux.conf".text = ''
 		set -g @catppuccin_flavor "mocha"
 		set -g @catppuccin_window_status_style "rounded"
